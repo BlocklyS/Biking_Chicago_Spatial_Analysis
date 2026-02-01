3101b6dff341e29b2e76d921e235daa2c8addec7
Sonia Mereu <123649352+BlocklyS@users.noreply.github.com>
2026-02-01 18:00:59 +0100

    Create cleaning_duration.sql

---
+++ sql/cleaning_duration.sql
@@ -0,0 +1,22 @@
+-- Data Sanitization --
+
+CREATE OR REPLACE TABLE
+  `project.cyclistic_data_analysis.ride_duration` AS
+SELECT
+  ride_id,
+  rideable_type,
+  member_casual,
+  TRIM(start_station_name) AS start_address,
+  TRIM(end_station_name) AS end_address,
+  start_lat,
+  start_lng,
+  end_lat,
+  end_lng,
+  started_at,
+  ended_at,
+  TIMESTAMP_DIFF(ended_at, started_at, MINUTE) AS ride_duration,
+  TIMESTAMP_DIFF(ended_at, started_at, SECOND) AS second_duration
+FROM `project.cyclistic_data_analysis.collected_cyclistic_data`
+WHERE TIMESTAMP_DIFF(ended_at, started_at, MINUTE)
+      BETWEEN 0 AND 1440;
+
