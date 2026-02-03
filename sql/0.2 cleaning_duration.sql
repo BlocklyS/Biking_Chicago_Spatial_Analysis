80a778f0209cbf57c22105b08a11ade0beaeecc0
Sonia Mereu <123649352+BlocklyS@users.noreply.github.com>
2026-02-03 11:45:04 +0100

    Update and rename cleaning_duration.sql to 0.2 cleaning_duration.sql

--- sql/0.2 cleaning_duration.sql
+++ sql/0.2 cleaning_duration.sql
@@ -1,7 +1,7 @@
 -- Purpose:
 -- Clean and normalize trip-level data before analysis.
 -- Remove temporally invalid rides, standardize station names, and compute ride duration.
---
+-- TRIM / TIMESTAMP_DIFF
 -- Key assumptions:
 -- - Valid rides last between 0 and 24 hours.
 --
@@ -28,4 +28,6 @@ SELECT
 FROM `project.cyclistic_data_analysis.collected_cyclistic_data`
 WHERE TIMESTAMP_DIFF(ended_at, started_at, MINUTE)
       BETWEEN 0 AND 1440;
+
+
 
